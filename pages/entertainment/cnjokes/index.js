621578cfdb2ca5a450bb0d4c9ca728293f9731c8
Ashwamedh Bahod <72094447+kickASS28@users.noreply.github.com>
2022-02-11 14:37:21 +0530

    Viewpoint meta

--- pages/entertainment/cnjokes/index.js
+++ pages/entertainment/cnjokes/index.js
@@ -61,6 +61,7 @@ const SoprtsPage = () => {
     <Fragment>
       <Head>
         <title>Joke Generator</title>
+        <meta name="viewport" content="width=device-width; initial-scale=1.0;" />
         <meta
           name="description"
           content="Laugh a lot with Chuck Norris jokes"
